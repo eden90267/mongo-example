7511160a3dd91349076fc860f95e1124c44641f3
eden90267 <eden90267@gmail.com>
2017-01-02 00:29:48 +0800

    add insert example

--- src/main/java/personal/eden/mongoexample/main.java
+++ src/main/java/personal/eden/mongoexample/main.java
@@ -1,8 +1,10 @@
 package personal.eden.mongoexample;
 
 import com.mongodb.*;
+import com.mongodb.util.JSON;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by eden90267 on 2017/1/1.
@@ -11,8 +13,11 @@ public class main {
 
     public static void main(String[] args) {
         MongoExample mongoExample = new MongoExample();
+
         mongoExample.openExample();
+        mongoExample.insertExample();
         mongoExample.closeExample();
+
     }
 
     private static class MongoExample {
@@ -36,14 +41,44 @@ public class main {
             coll = db.getCollection("test");
         }
 
+        public void insertExample() {
+            // String to JSON
+            String json_string = "{\"name\":\"MongoDB\",\"type\":\"database\",\"count\":1,\"info\":{\"x\":203,\"y\":102}}";
+            BasicDBObject doc = (BasicDBObject) JSON.parse(json_string);
+
+            coll.insert(doc);
+
+            // JSON object
+            BasicDBObject doc2 = new BasicDBObject();
+            doc2.put("name","MongoDB");
+            doc2.put("type","database");
+            doc2.put("count",2);
+
+            BasicDBObject info = new BasicDBObject();
+            info.put("x",203);
+            info.put("y",102);
+            doc2.put("info",info);
+
+            coll.insert(doc2);
+
+            // Multiple documents
+            String json_string3 = "{\"name\":\"MongoDB\",\"type\":\"database\",\"count\":3,\"info\":{\"x\":203,\"y\":102}}";
+            DBObject doc3 = (DBObject) JSON.parse(json_string3);
+
+            String json_string4 = "{\"name\":\"MongoDB\",\"type\":\"database\",\"count\":4,\"info\":{\"x\":203,\"y\":102}}";
+            DBObject doc4 = (DBObject) JSON.parse(json_string4);
+
+            List<DBObject> docs = Arrays.asList(doc3, doc4);
+
+            coll.insert(docs);
+
+        }
+
         public void closeExample() {
             mongoClient.close();
         }
 
     }
-
-
-
 
 
 }
